84ba601cb6f1203b899449cab33732ecc1858d61
student-manokamna <145145414+student-manokamna@users.noreply.github.com>
2024-11-22 18:51:35 +0530

    Delete file4.py

--- file4.py
+++
@@ -1,9 +0,0 @@
-
-''' 4. Write a program that asks the user for their name and then prints a
- greeting message '''
-
-
-name = input("What is your name? ")
-
-
-print("Hello,", name + " Welcome!!!!")
