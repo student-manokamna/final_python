da3d82e357c8d2ac31a517014c3acb6f58ebebbd
student-manokamna <145145414+student-manokamna@users.noreply.github.com>
2024-11-22 18:50:14 +0530

    Delete file10.py

--- file10.py
+++
@@ -1,9 +0,0 @@
-
-'''
-10. Write a python program that converts a given string to uppercase
-'''
-
-input_string = input("Enter a string: ")
-uppercase_string = input_string.upper()
-print("Uppercase string:", uppercase_string)
-
